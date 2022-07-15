7ad5fed773c72be9d8a4ae25116aeaebf6dba2e8
ErickRamses <105986183+ErickRamses@users.noreply.github.com>
2022-07-15 14:16:54 -0700

    header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div >
+      <div className="App">
+
+        <div className='Back'></div>
+        <div>
+
+          <div className='Header'>hi mogh add padding or somethong or quit scroll later</div>
+          <div className='Header1'>subtitle?</div>
+          <div className='Header2'>logo1</div>
+          <div className='Header3'>logo2</div>
+        </div>
+
+       
+
+      </div>
+      <div className='body'>
+        what i can do xd egtegnologies<br></br>
+        logo <br></br>
+        logo <br></br><br></br>                  
+        proyets
+        <br></br>
+        
+        <Card style={{ width: '320px',border:"0px" }} className="shadow">
+      <Card.Img variant="top" src="https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2017/03/space_debris/16871919-3-eng-GB/Space_Debris_pillars.jpg" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content. 
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary">git hub</Button>
+      </Card.Body>
+    </Card>
+      </div>
+      </div>
+    
   );
 }
 
