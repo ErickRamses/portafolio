f366f110baaba954a118eb3f1497d4736d8985c0
ErickRamses <105986183+ErickRamses@users.noreply.github.com>
2022-07-20 10:28:58 -0700

    optimitation1

--- src/componets/Canvas.jsx
+++ src/componets/Canvas.jsx
@@ -12,7 +12,7 @@ const Canvas = () => {
     canvasRef.current.height=window.innerHeight*1.1;
     canvasRef.current.width=document.body.scrollWidth;
     
-    let time =setInterval(space,1000/60)
+    let time =setInterval(space,1000/30)
     // do something here with the canvas
   }, [])
   window.addEventListener('resize',()=>{
@@ -25,6 +25,8 @@ const Canvas = () => {
   function mosemove(e){
     x=e.nativeEvent.offsetX
     y=e.nativeEvent.offsetY
+
+    //planets[0].massa=250
     //console.log(e)
     //ctx.fillStyle = '#000000'
     //ctx.beginPath()
@@ -277,9 +279,9 @@ class sun{
     }
     
 }
-suns[0]=new sun(0,125,-2010,200,1,0)
-for(let i=0;i<200;i++){
-    planets.push(new planet(i,Math.floor(Math.random() * (document.body.scrollWidth - 0) + 0),i*(window.innerHeight/200),0,0))
+suns[0]=new sun(0,250,-1000,0,1,0)
+for(let i=0;i<100;i++){
+    planets.push(new planet(i,Math.floor(Math.random() * (document.body.scrollWidth - 0) + 0),i*(window.innerHeight/100),0,0))
    // planets.push(new planet(i,100,i*(window.innerHeight/100),0,0))
    //Math.floor(Math.random() * (document.body.scrollWidth - 0 + 1) + 0);
     //console.log(Math.floor(Math.random() * (document.body.scrollWidth - 0) + 0))
