5834fd1f79cb07b367bf8fdd353ce11d6bdfac5a
ErickRamses <105986183+ErickRamses@users.noreply.github.com>
2022-10-16 13:14:17 -0700

    fadein

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@
 import './styles/main.css';
 import { Button, Container, Navbar } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Aos from 'aos';
+import "aos/dist/aos.css"
+
 import Card from 'react-bootstrap/Card';
 import html5 from "./imagen/html5.png"
 import css3 from "./imagen/css3.png"
@@ -66,6 +69,14 @@ function App() {
 
   let loaded=false;
   useEffect(()=>{
+
+    Aos.init({
+      duration:2000,
+      offset:-800,
+     once:true,
+    
+    })
+
     window.onscroll = function () { window.scrollTo(0, 0); };
     window.scrollTo(0, 0)
 
@@ -131,8 +142,8 @@ function App() {
       <div className='arrow-up'></div>
       <div className='body'>
       <br></br>
-       
-        <div className='Aboutme'>
+       <div style={{overflow:"hidden"}}>
+        <div className='Aboutme' data-aos="fade-left">
           <div style={{maxWidth:"700px"}}>
 
           <span style={{fontSize:"1.3em"}}>
@@ -142,6 +153,7 @@ function App() {
           </div>
          <img style={{width:"90%",maxWidth:"800px",borderRadius:"7px"}} loading={"lazy"} src={code}></img>
         </div>
+       </div>
       <div className='arrow-up2'></div>
         
         <div className='Skills'>
@@ -153,11 +165,20 @@ function App() {
             {/* I am proficient in these technologies. I can solve most problems. If I'm stuck in a situation, I can research the solution, and if I don't find it, it's a nice challenge to solve.    */}
             <div style={{display:"flex",justifyContent:"center"}}>
             <ul style={{textAlign:"left"}}>
-              <li><b>Responsive: </b>understanding of UI/UX best practices and responsive design</li>
+              <li><b>
+                <div data-aos-once="false" data-aos="fade-right" data-aos-offset="-550" data-aos-duration="500" data-aos-delay="0" style={{display:"inline-block"}}>Fast:</div>
+                </b>performance such as optimizing images and minifying CSS and JavaScript</li>
+              <li><b><div data-aos-once="false" data-aos="fade-down" data-aos-offset="-550" data-aos-duration="1500" data-aos-delay="50" style={{display:"inline-block"}}>Res</div>
+                      <div data-aos-once="false" data-aos="fade-down" data-aos-offset="-550" data-aos-duration="1500" data-aos-delay="100" style={{display:"inline-block"}}>pons</div> 
+                      <div data-aos-once="false" data-aos="fade-down" data-aos-offset="-550" data-aos-duration="1500" data-aos-delay="150" style={{display:"inline-block"}}>ive:</div> 
+                      </b>understanding of UI/UX best practices and responsive design</li>
               {/* <li>excellent debugging skills with dev tools and coding best practices</li> */}
-              <li><b>Fast: </b>performance such as optimizing images and minifying CSS and JavaScript</li>
-              <li><b>Dynamic: </b>websites can be much more interesting , i love making pages come to life.</li>
-
+              <li><b>
+                <div data-aos-once="false" data-aos="flip-up" data-aos-offset="-550" data-aos-duration="2000" data-aos-delay="500" style={{display:"inline-block"}}>Dy</div>
+                <div data-aos-once="false" data-aos="fade-up-left" data-aos-offset="-550" data-aos-duration="1000" data-aos-delay="500" style={{display:"inline-block"}}>na</div>
+                <div data-aos-once="false" data-aos="fade-up-right" data-aos-offset="-550" data-aos-duration="500" data-aos-delay="500" style={{display:"inline-block"}}>mic:</div>
+              </b>
+              websites can be much more interesting , i love making pages come to life.</li>
             </ul>
             </div>
 
@@ -167,7 +188,7 @@ function App() {
             <br></br>
             <br></br>
 
-          <div style={{gap:"10px"}} className='d-flex flex-wrap justify-content-center'>
+          <div style={{gap:"10px"}} className='d-flex flex-wrap justify-content-center' data-aos="fade-zoom-in">
           <div style={{fontSize:"1.5em",width:"100%",textAlign:"center",}}>
           <span style={{fontSize:"1.3em"}}>
           </span>
@@ -176,22 +197,22 @@ function App() {
           <br></br>
             {/* <div><img className='imagen' src={html5}></img><div style={{textAlign:"center"}}>html5</div></div>
             <div><img className='imagen' src={css3}></img><div style={{textAlign:"center"}}>Css3</div></div> */}
-            <div><img className='imagenround' src={javas}></img><div style={{textAlign:"center"}}>JavaScript (ES6+)</div></div>
-            <div><div style={{width:"90px",height:"90px",display:"flex",justifyContent:"center",alignItems:"center"}}><img style={{width:"84px"}} className='imagen' src={ts}></img></div><div style={{textAlign:"center"}}>Typescript</div></div>
-            <div><img className='imagenround' src={reactl}></img><div style={{textAlign:"center"}}>React</div></div>
-            <div><img className='imagen' src={next}></img><div style={{textAlign:"center"}}>Next</div></div>
+            <div data-aos-offset="-550" data-aos="fade-left" data-aos-delay="600"><img className='imagenround' src={javas}></img><div style={{textAlign:"center"}}>JavaScript (ES6+)</div></div>
+            <div data-aos-offset="-550" data-aos="fade-left" data-aos-delay="650"><div style={{width:"90px",height:"90px",display:"flex",justifyContent:"center",alignItems:"center"}}><img style={{width:"84px"}} className='imagen' src={ts}></img></div><div style={{textAlign:"center"}}>Typescript</div></div>
+            <div data-aos-offset="-550" data-aos="fade-left" data-aos-delay="700"><img className='imagenround' src={reactl}></img><div style={{textAlign:"center"}}>React</div></div>
+            <div data-aos-offset="-550" data-aos="fade-left" data-aos-delay="750"><img className='imagen' src={next}></img><div style={{textAlign:"center"}}>Next</div></div>
            
 
 
-            <div><img className='imagen' src={bootstrap}></img><div style={{textAlign:"center"}}>Bootstrap</div></div>
-            <div><img className='imagen' src={sass}></img><div style={{textAlign:"center"}}>Sass</div></div>
+            <div data-aos-offset="-550" data-aos="fade-left" data-aos-delay="800"><img className='imagen' src={bootstrap}></img><div style={{textAlign:"center"}}>Bootstrap</div></div>
+            <div data-aos-offset="-550" data-aos="fade-left" data-aos-delay="850"><img className='imagen' src={sass}></img><div style={{textAlign:"center"}}>Sass</div></div>
 
 
-           <div><img className='imagen imagenround' src={axi}></img><div style={{textAlign:"center"}}>Axios</div></div>     
-           <div><img className='imagen' src={red}></img><div style={{textAlign:"center"}}>Redux</div></div>     
-            <div><img className='imagenround' src={reactrouterl}></img><div style={{textAlign:"center"}}>React Router</div></div> 
+           <div data-aos-offset="-550" data-aos="fade-left" data-aos-delay="900"><img className='imagen imagenround' src={axi}></img><div style={{textAlign:"center"}}>Axios</div></div>     
+           <div data-aos-offset="-550" data-aos="fade-left" data-aos-delay="950"><img className='imagen' src={red}></img><div style={{textAlign:"center"}}>Redux</div></div>     
+            <div data-aos-offset="-550" data-aos="fade-left" data-aos-delay="1000"><img className='imagenround' src={reactrouterl}></img><div style={{textAlign:"center"}}>React Router</div></div> 
             {/* <div><img className='imagenround' src={reactrouterl}></img><div style={{textAlign:"center"}}>React Router</div></div> */}
-            <div><img className='imagen' src={git}></img><div style={{textAlign:"center"}}>Git</div></div>     
+            <div data-aos-offset="-550" data-aos="fade-left" data-aos-delay="1050"><img className='imagen' src={git}></img><div style={{textAlign:"center"}}>Git</div></div>     
           </div >
           <br></br>
 
@@ -211,11 +232,11 @@ function App() {
             <div style={{gap:"10px"}} className='d-flex flex-wrap justify-content-center'>
             
 
-  <div><div style={{width:"90px",height:"90px",display:"flex",justifyContent:"center",alignItems:"center"}}><img style={{width:"84px",borderRadius:"10px"}} className='imagen' src={mon}></img></div><div style={{textAlign:"center"}}>Mongodb</div></div>
-  <div><div style={{width:"90px",height:"90px",display:"flex",justifyContent:"center",alignItems:"center"}}><img style={{width:"84px"}} className='imagen' src={node}></img></div><div style={{textAlign:"center"}}>Node</div></div>
-  <div><div style={{width:"90px",height:"90px",display:"flex",justifyContent:"center",alignItems:"center"}}><img style={{width:"84px",borderRadius:"10px"}} className='imagen' src={ex}></img></div><div style={{textAlign:"center"}}>Express</div></div>
-  <div><div style={{width:"90px",height:"90px",display:"flex",justifyContent:"center",alignItems:"center"}}><img style={{width:"180px"}} className='imagen' src={graph}></img></div><div style={{textAlign:"center"}}>GraphQL</div></div>
-            <div><img className='imagen' src={mog}></img><div style={{textAlign:"center"}}>Mongoose</div></div>     
+  <div data-aos-offset="-600" data-aos="fade-right" data-aos-delay="1050"><div style={{width:"90px",height:"90px",display:"flex",justifyContent:"center",alignItems:"center"}}><img style={{width:"84px",borderRadius:"10px"}} className='imagen' src={mon}></img></div><div style={{textAlign:"center"}}>Mongodb</div></div>
+  <div data-aos-offset="-600" data-aos="fade-right" data-aos-delay="1150"><div style={{width:"90px",height:"90px",display:"flex",justifyContent:"center",alignItems:"center"}}><img style={{width:"84px"}} className='imagen' src={node}></img></div><div style={{textAlign:"center"}}>Node</div></div>
+  <div data-aos-offset="-600" data-aos="fade-right" data-aos-delay="1250"><div style={{width:"90px",height:"90px",display:"flex",justifyContent:"center",alignItems:"center"}}><img style={{width:"84px",borderRadius:"10px"}} className='imagen' src={ex}></img></div><div style={{textAlign:"center"}}>Express</div></div>
+  <div data-aos-offset="-600" data-aos="fade-right" data-aos-delay="1350"><div style={{width:"90px",height:"90px",display:"flex",justifyContent:"center",alignItems:"center"}}><img style={{width:"180px"}} className='imagen' src={graph}></img></div><div style={{textAlign:"center"}}>GraphQL</div></div>
+            <div data-aos-offset="-600" data-aos="fade-right" data-aos-delay="1450"><img className='imagen' src={mog}></img><div style={{textAlign:"center"}}>Mongoose</div></div>     
              
           </div>          
         </div></div>
@@ -231,17 +252,24 @@ function App() {
           <br></br>
           <br></br>
           <div style={{gap:"10px" }} className='d-flex flex-wrap justify-content-center'>
-           <Projects video={grav} video2={grav2} video3={grav3} title={"Gravity simulator"} info={" with features like mass, time warp and more, i build this to get better at javascript i used classes for suns and planets and an array for trajectories it is a complex project that got me studying trigonometry it was hard and fun to build and debug"} live={"/Grav"} code={"https://github.com/ErickRamses/grav"}></Projects>
-
-           <Projects video={sing} video2={sing2} video3={sing3} title={"MERN Budgets"} info={"a fullstack aplication where you can make budgets and save it if you login , made with the mern stack ,the server is a rest Api that uses authentication to receive and update information ,backend is hosted in heroku"} live={"https://beamish-paletas-1906d1.netlify.app/"} code={"https://github.com/ErickRamses/client"}  two={true}></Projects>
-
-           <Projects video={ApiCountries} video2={ApiCountries2} video3={ApiCountries3} title={"Countries Api"} info={"i build this website with react and react router it fetches from an api and displays the information in an organized way you can search by name and filter by region also you can click on a country to see more detailed information on a separate page i used local storage and useEfect to load faster for filter i used 2 useState "} live={"/ApiCountries"} code={"https://github.com/ErickRamses/rest-countries-api-with-color-theme-switcher-master"}></Projects>  
-
-           <Projects video={sp1} video2={sp2} video3={sp3} title={"Space tourism"} info={"a landing website to get information about space tourism,i build this because i thought it was an interesting topic and i wanted to challenge my self to do it as fast as posible , this time i used scroll() to navigate"} live={"/space"} code={"https://github.com/ErickRamses/space-tourism"}></Projects>  
-
-           <Projects video={name1} video2={name2} video3={name3} title={"Name Api"} info={" i build this website with html css javascript and sass as you can see you can write a name and get a probability of where is from i get this information from a public API also you can toggle dark mode i did it by changing the values of css variables "} live={"/Name"} code={"https://github.com/ErickRamses/name"}></Projects>  
-
-           <Projects video={p1} video2={p2} video3={p3} title={"PingPong"} info={"a game of pingpong with an AI with dynamic difficulty made with html css javascript you can also play 2 players mode , to make dificulty dynamic , the reaction time of the AI is modified "} live={"/PingPong"} code={"https://github.com/ErickRamses/pingpong"}></Projects> 
+           <div data-aos="fade-up" data-aos-offset="-650" data-aos-duration="1000"  >
+              <Projects video={grav} video2={grav2} video3={grav3} title={"Gravity simulator"} info={" with features like mass, time warp and more, i build this to get better at javascript i used classes for suns and planets and an array for trajectories it is a complex project that got me studying trigonometry it was hard and fun to build and debug"} live={"/Grav"} code={"https://github.com/ErickRamses/grav"}></Projects>
+           </div>
+           <div data-aos="fade-up" data-aos-offset="-650" data-aos-duration="1000">    
+              <Projects video={sing} video2={sing2} video3={sing3} title={"MERN Budgets"} info={"a fullstack aplication where you can make budgets and save it if you login , made with the mern stack ,the server is a rest Api that uses authentication to receive and update information ,backend is hosted in heroku"} live={"https://beamish-paletas-1906d1.netlify.app/"} code={"https://github.com/ErickRamses/client"}  two={true}></Projects>
+           </div>
+           <div data-aos="fade-up" data-aos-offset="-650" data-aos-duration="1000">
+              <Projects video={ApiCountries} video2={ApiCountries2} video3={ApiCountries3} title={"Countries Api"} info={"i build this website with react and react router it fetches from an api and displays the information in an organized way you can search by name and filter by region also you can click on a country to see more detailed information on a separate page i used local storage and useEfect to load faster for filter i used 2 useState "} live={"/ApiCountries"} code={"https://github.com/ErickRamses/rest-countries-api-with-color-theme-switcher-master"}></Projects>   
+           </div>
+           <div data-aos="fade-up" data-aos-offset="-650" data-aos-duration="1000">   
+              <Projects video={sp1} video2={sp2} video3={sp3} title={"Space tourism"} info={"a landing website to get information about space tourism,i build this because i thought it was an interesting topic and i wanted to challenge my self to do it as fast as posible , this time i used scroll() to navigate"} live={"/space"} code={"https://github.com/ErickRamses/space-tourism"}></Projects>  
+           </div>
+           <div data-aos="fade-up" data-aos-offset="-650" data-aos-duration="1000">
+              <Projects video={name1} video2={name2} video3={name3} title={"Name Api"} info={" i build this website with html css javascript and sass as you can see you can write a name and get a probability of where is from i get this information from a public API also you can toggle dark mode i did it by changing the values of css variables "} live={"/Name"} code={"https://github.com/ErickRamses/name"}></Projects>  
+           </div>
+           <div data-aos="fade-up" data-aos-offset="-650" data-aos-duration="1000">
+              <Projects video={p1} video2={p2} video3={p3} title={"PingPong"} info={"a game of pingpong with an AI with dynamic difficulty made with html css javascript you can also play 2 players mode , to make dificulty dynamic , the reaction time of the AI is modified "} live={"/PingPong"} code={"https://github.com/ErickRamses/pingpong"}></Projects> 
+           </div>
           
            
 
